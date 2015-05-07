2da722076a217bdf9053deffd409a36aeff13412
Anthony Yu <patilise@gmail.com>
2015-05-07 15:09:17 +0800

    New: Game 4

---
+++ public/js/game4_game.js
@@ -0,0 +1,39 @@
+﻿var MIN_ACCE = 1;
+var NUM_OF_PLAYERS = 4;
+
+function showResult() {
+    isPlaying = false;
+    
+    for (var i = 0; i < NUM_OF_PLAYERS; ++i) {
+        if (!clientExists[i]) {
+            var element = document.getElementById('GameResult' + (i+1));
+            element.setAttribute('class', 'btn btn-lg btn-remote');
+            element.innerHTML = '<i class="fa fa-smile-o"></i>';
+        } else if (endTime[i] === 0) {
+            var element = document.getElementById('GameResult' + (i+1));
+            element.setAttribute('class', 'btn btn-lg btn-remote btn-success');
+            element.textContent = 'ALIVE (' + 10 + ' seconds)';
+        } else {
+            var element = document.getElementById('GameResult' + (i+1));
+            element.setAttribute('class', 'btn btn-lg btn-remote btn-danger');
+            element.textContent = 'DEAD (' + round((endTime[i] - startTime)/1000) + ' seconds)';
+        }
+    }
+    
+    document.getElementById('SwingText').setAttribute('class', 'txt-swing hidden');
+    document.getElementById('StartGroup').setAttribute('class', 'form-group');
+    document.getElementById('StartButton').textContent = 'Restart';
+    console.log('Finished showResult()');
+}
+
+function deviceMotionHandler(eventData) {
+    var acceZ = eventData.acceleration.z;
+    if (acceZ >= MIN_ACCE) {
+        sendSwing(acceZ);
+    }
+}
+
+function round(val) {
+    var amt = 10;
+    return Math.round(val * amt) / amt;
+}
---
+++ public/js/game4_session.js
@@ -0,0 +1,102 @@
+﻿var socket = io('ws://' + window.location.hostname + ':8000/');
+var sessionId = null;
+var clientId = null;
+var endTime = [0, 0, 0, 0];
+var startTime;
+var clientExists = [false, false, false, false];
+var isPlaying = false;
+
+socket.on('register', function(sId, cId) {
+    console.log('Received register: ' + sId + ' ' + cId);
+});
+socket.on('swing', function(recvClientId, data, time) {
+    if (data == -1) {
+        clientExists[recvClientId-1] = true;
+        return;
+    }
+    
+    if (!isPlaying || clientId != 0 || endTime[recvClientId-1] != 0) {
+        console.log('Received swing: from id ' + recvClientId + ', ' + data + ', at time ' + time + ', discarded');
+        return;
+    }
+    
+    var clientTime = parseInt(time);
+    var clientData = parseFloat(data);
+    endTime[recvClientId] = Date.now();
+    console.log('Received swing: from id ' + recvClientId + ', ' + data + ', at time ' + time);
+    
+    //TODO: change win/lose button
+});
+
+function sendSwing(swing) {
+    console.log('Send swing: ' + swing);
+    socket.emit('swing', clientId, swing, Date.now());
+}
+
+function getSessionId() {
+    var pageURL = document.URL;
+    var n = pageURL.search(/\/session_dontmove\/.*\//);
+    if (n != -1) {
+        return parseInt(pageURL.substring(n + 18, pageURL.length - 2));
+    } else {
+        return null;
+    }
+}
+
+function getClientId() {
+    var pageURL = document.URL;
+    var clientId = parseInt(pageURL.substr(pageURL.length - 1));
+    if (isNaN(clientId))
+        return null;
+    else
+        return clientId;
+}
+
+function registerSession() {
+    var sId = getSessionId();
+    var cId = getClientId();
+    if (sId !== null && cId !== null) {
+        socket.emit('register', sId, cId);
+        console.log('Sent register message: ' + sId + ' ' + cId);
+    } else {
+        console.log('Session ID and/or client ID is null!');
+    }
+}
+
+function startGame_ScreenSide() {
+    isPlaying = true;
+    clientScore = [0, 0, 0, 0];
+    document.getElementById('GameColumn').setAttribute('class', 'col-12');
+    document.getElementById('GameButtons').setAttribute('class', 'hidden');
+    
+    for (var i = 1; i <= NUM_OF_PLAYERS; i++) {
+        var element = document.getElementById('GameResult' + i);
+        element.setAttribute('class', 'btn btn-lg btn-remote btn-warning');
+        element.textContent = 'ZEN';
+        
+        endTime[i-1] = 0;
+    }
+    document.getElementById('QRGroup').setAttribute('class', 'hidden');
+    document.getElementById('StartGroup').setAttribute('class', 'form-group hidden');
+    document.getElementById('SwingText').setAttribute('class', 'txt-swing');
+    //TODO: Start video
+    
+    setTimeout(showResult, 10000);
+    console.log("Start game");
+}
+
+function init() {
+    sessionId = getSessionId();
+    clientId = getClientId();
+    registerSession();
+    if (clientId != 0) {
+        if ((window.DeviceMotionEvent) || ('listenForDeviceMovement' in window)) {
+            window.addEventListener('devicemotion', deviceMotionHandler, false);
+        } else {
+            console.log('Not supported on your device or browser.  Sorry.');
+        }
+    }
+    sendSwing(-1);
+}
+
+window.addEventListener('load', init);
